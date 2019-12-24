8e665eb391fd2395de1336b3cf68f6a0be4ed304
Jean-Luc Etienne <jletienne@jean-lucs-mbp.lan>
2019-12-24 11:30:28 -0600

    update rawdata

--- get_stats.py
+++ get_stats.py
@@ -5,6 +5,7 @@ import json
 from json import dumps
 import datetime
 
+week_num = 16
 
 class Yahoo_Api():
     def __init__(self,
@@ -37,23 +38,26 @@ yahoo_api._login()
 weekly_team_stats = []
 
 opponent = {'0': '1', '1': '0'}
-for week in range(1, 14):
+for week in range(1, week_num+1):
     url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/390.l.227235/scoreboard;week={}'.format(week)
     response = oauth.session.get(url, params={'format': 'json'})
     r = response.json()
 
     for game in ['0', '1', '2', '3', '4']:
 
-        for team in ['0','1']:
-            name = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][0][2]['name']
-            manager = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][0][-1]['managers'][0]['manager']['nickname']
-            points = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][1]['team_points']['total']
-            points_against = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][opponent[team]]['team'][1]['team_points']['total']
+        try:
+            for team in ['0','1']:
+                name = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][0][2]['name']
+                manager = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][0][-1]['managers'][0]['manager']['nickname']
+                points = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][1]['team_points']['total']
+                points_against = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][opponent[team]]['team'][1]['team_points']['total']
 
-            projected_points = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][1]['team_projected_points']['total']
+                projected_points = r['fantasy_content']['league'][1]['scoreboard']['0']['matchups'][game]['matchup']['0']['teams'][team]['team'][1]['team_projected_points']['total']
 
-            stats={'week': week, 'manager': manager, 'team_name': name, 'points': points, 'points_against': points_against, 'projected_points': projected_points}
+                stats={'week': week, 'manager': manager, 'team_name': name, 'points': points, 'points_against': points_against, 'projected_points': projected_points}
 
-            weekly_team_stats.append(stats)
+                weekly_team_stats.append(stats)
+        except:
+            pass
 
 pd.DataFrame(weekly_team_stats).to_csv("rawdata/2019_fantasy_stats.csv", index=None)
